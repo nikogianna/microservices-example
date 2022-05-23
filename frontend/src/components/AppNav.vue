4e0b62a676c6410e1cbe723264e39eb68dbfe9ec
nikogianna <nikogianna19@gmail.com>
2022-05-23 15:08:40 +0200

    Update AppNav.vue

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -1,7 +1,7 @@
 
 <template>
   <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-    <a class="navbar-brand" href="#">70d0 0v3r3n61n33r1n</a>
+    <a class="navbar-brand" href="#">70d0 0v3r3n61n33rn</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item" v-show="isAdmin()">
         <a class="btn btn-success mr-sm-2" href="#/admin">Admin</a>
