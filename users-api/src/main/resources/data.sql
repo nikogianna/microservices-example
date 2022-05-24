e3aa6110083bc9ca0b878988084e2e55a3c223dc
nikogianna <nikogianna19@gmail.com>
2022-05-24 15:12:12 +0200

    Update data.sql

--- users-api/src/main/resources/data.sql
+++ users-api/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO users (username, firstname, lastname, role) VALUES
   ('admin', 'Foo', 'Bar', 1),
   ('johnd', 'John', 'Doe', 0),
+  ('niko', 'Niko', 'Doe', 1),
   ('janed', 'Jane', 'Doe', 0);
